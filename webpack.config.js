0191bb372660f03b1d0d6fb37e0408088fcce99d
John Connor <jcm10@rice.edu>
2023-10-02 15:12:28 -0500

    dirname bug? i really don't know this framework

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const path = require('path');
 
 module.exports = {
